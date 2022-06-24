d9b837266c232aaa8ea97ca608a69116a23b94ce
Taj <betetat94@gmail.com>
2022-06-23 21:13:29 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArgs.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const generatePage = require('./src/page-template.js');
 
-    console.log('==================');
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArgs.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('==================');
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
 
